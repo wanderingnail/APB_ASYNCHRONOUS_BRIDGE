e23332806440e1782cc5f82be24d4f1cb8a5daf5
wandering_nail <1754323999@qq.com>
2022-11-18 11:15:19 +0800

    modified:   rtl/slave.v

--- rtl/slave.v
+++ rtl/slave.v
@@ -20,19 +20,18 @@ module slave #(
 
 localparam ADDR_WD_L = ADDR_WD - 2;
 
-reg [DATA_WD-1 : 0] mem [(1 << ADDR_WD_L)-1 : 0];
+reg  [DATA_WD-1 : 0] mem [(1 << ADDR_WD_L)-1 : 0];
 
 wire [ADDR_WD_L-1 : 0] b_paddr_L;
 
 assign b_paddr_L = b_paddr[ADDR_WD_L-1 : 0];
-assign b_pready = 1;
+assign b_prdata  = mem[b_paddr_L];
+assign b_pready  = 1'b1;
 
 always @(posedge b_pclk or negedge b_prst_n ) begin
     if (b_penable && b_pwrite && b_pready) begin
         mem[b_paddr_L] <= b_pwdata;
     end
 end
-
-assign b_prdata = mem[b_paddr_L];
 
 endmodule
